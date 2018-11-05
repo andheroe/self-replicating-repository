f45ed337f8333b81c0aa22484e4221c38ff4f948
andheroe <freefunning@gmail.com>
2018-11-05 14:47:51 +0200

    Committing file 27 of 37 for andheroe to self-replicating-repoSITORY: client/containers/home.jsx

---
+++ client/containers/home.jsx
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Link } from 'react-router-dom';
+
+// Import action creators
+import { loadTokenAndGetUser, logout } from '../actions/index';
+
+// Import selectors
+import {
+  githubAuthTokenSelector,
+  isLoggedInSelector,
+  loginErrorSelector,
+  githubUserDataSelector
+} from '../selectors/index';
+
+// Import components
+import { WelcomeMessage } from '../components/welcomeMessage.jsx';
+import CopyAppToRepo from './copyAppToRepo.jsx';
+
+class Home extends Component {
+  componentDidMount() {
+    this.props.loadTokenAndGetUser();
+  }
+
+  renderBody = () => {
+    if (this.props.isLoggedIn) {
+      const bodyStyle = {
+        minHeight: '300px',
+        paddingBottom: '80px',
+      };
+
+      return (
+        <div className="container">
+          <div className="row" style={bodyStyle}>
+            <CopyAppToRepo/>
+          </div>
+          <div className="row justify-content-center">
+            <div className="col-12 text-center">
+              <button
+                className="btn btn-link "
+                onClick={this.props.logout}
+                >Disconnect GitHub Account
+              </button>
+            </div>
+          </div>
+        </div>
+      );
+    } else if (true) {
+      return (
+        <div></div>
+      );
+    }
+  };
+
+  render() {
+    const { isLoggedIn, user, logout } = this.props;
+
+    const containerStyle = {
+      maxWidth: '550px',
+      marginTop: '50px',
+      marginBottom: '50px',
+    };
+
+    return (
+      <main role="main" className="container" style={containerStyle}>
+        <div style={{paddingBottom: '30px'}}>
+          <WelcomeMessage
+            isLoggedIn={this.props.isLoggedIn}
+            loginError={this.props.loginError}
+            userData={this.props.user}
+            />
+        </div>
+        {this.renderBody(isLoggedIn, logout)}
+      </main>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    token: githubAuthTokenSelector(state),
+    loginError: loginErrorSelector(state),
+    isLoggedIn: isLoggedInSelector(state),
+    user: githubUserDataSelector(state),
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ loadTokenAndGetUser, logout }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
